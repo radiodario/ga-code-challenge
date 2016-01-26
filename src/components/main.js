470cada9b3276761560bad7a02542fbf8f7ed905
radiodario <dario.villanueva@gmail.com>
2016-01-26 23:42:13 +0000

    header and some more styles

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,13 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="app">
-        <h1>General Assembly Movie Search Engine</h1>
+        <div className="header">
+          <h1 className="header__title">General Assembly Movie Search Engine</h1>
+          <ul className="header__links">
+            <li><Link to={'/search'}>Search</Link></li>
+            <li><Link to={'/favorites'}>Favorites</Link></li>
+          </ul>
+        </div>
         <div className="main">
           {this.props.children}
         </div>
