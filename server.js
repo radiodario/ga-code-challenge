eb50ff25be43f5c4306c56078a161d663b720ff5
radiodario <dario.villanueva@gmail.com>
2016-01-26 15:26:51 +0000

    fix server issues

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/favorites', function(req, res){
+  var data = fs.readFileSync('./data.json');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
+
+app.post('/favorites', function(req, res) {
+  if(!req.body.name || !req.body.oid){
+    res.send("Error");
+    return
+  }
+
+  var data = JSON.parse(fs.readFileSync('./data.json'));
+  data.push(req.body);
+  fs.writeFile('./data.json', JSON.stringify(data));
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port 3000");
+});
